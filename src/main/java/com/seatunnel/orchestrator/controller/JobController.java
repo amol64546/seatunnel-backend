71ea4d83406f5af92fec92d2562c0bad9345133b
Amol N <amol64546@gmail.com>
2026-04-10 17:46:54 +0530

    refactor package

---
+++ src/main/java/com/seatunnel/orchestrator/controller/JobController.java
@@ -0,0 +1,59 @@
+package com.seatunnel.orchestrator.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.seatunnel.orchestrator.enums.JobStatus;
+import com.seatunnel.orchestrator.model.ETLJobOverview;
+import com.seatunnel.orchestrator.model.ETLJobStatus;
+import com.seatunnel.orchestrator.service.EtlJobService;
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("")
+@RequiredArgsConstructor
+@Slf4j
+public class JobController {
+
+  private final EtlJobService etlJobService;
+
+  @GetMapping(value = "/{jobId}", produces = {"application/json"})
+  public ResponseEntity<ETLJobStatus> getJobsById(@PathVariable String jobId) {
+    return ResponseEntity.ok().body(etlJobService.getJobsById(jobId));
+  }
+
+  @GetMapping(produces = {"application/json"})
+  public ResponseEntity<Object> getJobsByStatus(@RequestParam JobStatus status) {
+    return ResponseEntity.ok().body(etlJobService.getJobsByStatus(status));
+  }
+
+  @GetMapping(value = "/overview", produces = {"application/json"})
+  public ResponseEntity<ETLJobOverview> getJobsOverview() {
+    return ResponseEntity.ok().body(etlJobService.getJobsOverview());
+  }
+
+  @PostMapping(value = "/stop/{jobId}", produces = {"application/json"})
+  public ResponseEntity<Map<String, String>> stopJob(@PathVariable String jobId,
+                                                     @RequestParam(required = false) boolean isStopWithSavePoint) {
+    log.info("POST:/v1.0/pipeline/etl/stop-job?jobId={}&isStopWithSavePoint={}", jobId,
+      isStopWithSavePoint);
+    return ResponseEntity.ok(etlJobService.stopJob(jobId, isStopWithSavePoint));
+  }
+
+  @GetMapping(value = "/{id}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<String> streamJobStatus(@PathVariable String id) {
+    return etlJobService.streamJobStatus(id);
+  }
+
+  @Hidden
+  @PostMapping(value = "/callback", consumes = "application/json")
+  public void jobStatusCallback(@RequestBody JsonNode event) {
+    etlJobService.callback(event);
+  }
+}
--- src/main/java/com/seatunnel/orchestrator/controller/PipelineController.java
+++ src/main/java/com/seatunnel/orchestrator/controller/PipelineController.java
@@ -1,24 +1,16 @@
 package com.seatunnel.orchestrator.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.seatunnel.orchestrator.enums.JobStatus;
-import com.seatunnel.orchestrator.model.ETLJobOverview;
 import com.seatunnel.orchestrator.model.ETLJobStatus;
 import com.seatunnel.orchestrator.model.EtlPipeline;
 import com.seatunnel.orchestrator.service.EtlJobService;
 import com.seatunnel.orchestrator.service.EtlPipelineService;
-import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.Flux;
 
-import java.time.Duration;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -72,38 +64,4 @@ public class PipelineController {
       etlJobService.executePipeline(id, jobId, env));
   }
 
-  @GetMapping(value = "/jobs/{jobId}", produces = {"application/json"})
-  public ResponseEntity<ETLJobStatus> getJobsById(@PathVariable String jobId) {
-    return ResponseEntity.ok().body(etlJobService.getJobsById(jobId));
-  }
-
-  @GetMapping(value = "/jobs", produces = {"application/json"})
-  public ResponseEntity<Object> getJobsByStatus(@RequestParam JobStatus status) {
-    return ResponseEntity.ok().body(etlJobService.getJobsByStatus(status));
-  }
-
-  @GetMapping(value = "/jobs/overview", produces = {"application/json"})
-  public ResponseEntity<ETLJobOverview> getJobsOverview() {
-    return ResponseEntity.ok().body(etlJobService.getJobsOverview());
-  }
-
-  @PostMapping(value = "/jobs/stop/{jobId}", produces = {"application/json"})
-  public ResponseEntity<Map<String, String>> stopJob(@PathVariable String jobId,
-                                                     @RequestParam(required = false) boolean isStopWithSavePoint) {
-    log.info("POST:/v1.0/pipeline/etl/stop-job?jobId={}&isStopWithSavePoint={}", jobId,
-      isStopWithSavePoint);
-    return ResponseEntity.ok(etlJobService.stopJob(jobId, isStopWithSavePoint));
-  }
-
-  @Hidden
-  @PostMapping(value = "/jobs/callback", consumes = "application/json")
-  public void jobStatusCallback(@RequestBody JsonNode event) {
-    etlJobService.callback(event);
-  }
-
-
-  @GetMapping(value = "/jobs/{id}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<String> streamJobStatus(@PathVariable String id) {
-    return etlJobService.streamJobStatus(id);
-  }
 }
