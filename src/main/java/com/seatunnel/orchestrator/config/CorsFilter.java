d8b2310defa28b73018b516d87237c560f509154
Amol N <amol64546@gmail.com>
2026-04-16 12:36:08 +0530

    corseFilter

---
+++ src/main/java/com/seatunnel/orchestrator/config/CorsFilter.java
@@ -0,0 +1,37 @@
+
+package com.seatunnel.orchestrator.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                  FilterChain filterChain) throws ServletException, IOException {
+    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN,
+      request.getHeader(HttpHeaders.ORIGIN));
+    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "POST, GET, OPTIONS, DELETE, PUT");
+    response.setHeader(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
+    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS,
+      "Content-Type, Accept, X-Requested-With, remember-me, Authorization");
+
+    if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+      response.setStatus(HttpServletResponse.SC_OK);
+    } else {
+      filterChain.doFilter(request, response);
+    }
+  }
+}
